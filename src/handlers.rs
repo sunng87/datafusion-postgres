9d3c96c5ec7cf8fcbcdb257f83f8ee01cd7f95d9
Ning Sun <sunng@protonmail.com>
2024-04-05 21:29:03 -0700

    feat: initial attempt for extended query handler

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,11 +1,18 @@
 use std::sync::Arc;
 
 use async_trait::async_trait;
+use datafusion::logical_expr::LogicalPlan;
 use datafusion::prelude::*;
-use pgwire::api::query::SimpleQueryHandler;
-use pgwire::api::results::{Response, Tag};
-use pgwire::api::ClientInfo;
+use pgwire::api::portal::Portal;
+use pgwire::api::query::{ExtendedQueryHandler, SimpleQueryHandler};
+use pgwire::api::results::{
+    DescribePortalResponse, DescribeStatementResponse, FieldInfo, QueryResponse, Response, Tag,
+};
+use pgwire::api::stmt::QueryParser;
+use pgwire::api::stmt::StoredStatement;
+use pgwire::api::{ClientInfo, Type};
 use pgwire::error::{ErrorInfo, PgWireError, PgWireResult};
+
 use tokio::sync::Mutex;
 
 use crate::datatypes::encode_dataframe;
@@ -59,5 +66,62 @@ impl SimpleQueryHandler for DfSessionService {
                 "Datafusion is a readonly execution engine. To load data, call\nLOAD json_file_path table_name;".to_owned(),
             )))])
         }
+    }
+}
+
+pub(crate) struct Parser;
+
+#[async_trait]
+impl QueryParser for Parser {
+    type Statement = LogicalPlan;
+
+    async fn parse_sql(&self, sql: &str, types: &[Type]) -> PgWireResult<Self::Statement> {
+        todo!()
+    }
+}
+
+#[async_trait]
+impl ExtendedQueryHandler for DfSessionService {
+    type Statement = LogicalPlan;
+
+    type QueryParser = Parser;
+
+    #[doc = " Get a reference to associated `QueryParser` implementation"]
+    fn query_parser(&self) -> Arc<Self::QueryParser> {
+        todo!()
+    }
+
+    async fn do_describe_statement<C>(
+        &self,
+        client: &mut C,
+        target: &StoredStatement<Self::Statement>,
+    ) -> PgWireResult<DescribeStatementResponse>
+    where
+        C: ClientInfo + Unpin + Send + Sync,
+    {
+        todo!()
+    }
+
+    async fn do_describe_portal<C>(
+        &self,
+        client: &mut C,
+        target: &Portal<Self::Statement>,
+    ) -> PgWireResult<DescribePortalResponse>
+    where
+        C: ClientInfo + Unpin + Send + Sync,
+    {
+        todo!()
+    }
+
+    async fn do_query<'a, C>(
+        &self,
+        client: &mut C,
+        portal: &'a Portal<Self::Statement>,
+        max_rows: usize,
+    ) -> PgWireResult<Response<'a>>
+    where
+        C: ClientInfo + Unpin + Send + Sync,
+    {
+        todo!()
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::sync::Arc;
 
 use pgwire::api::auth::noop::NoopStartupHandler;
-use pgwire::api::query::PlaceholderExtendedQueryHandler;
 use pgwire::api::{MakeHandler, StatelessMakeHandler};
 use pgwire::tokio::process_socket;
 use tokio::net::TcpListener;
@@ -14,10 +13,6 @@ async fn main() {
     let processor = Arc::new(StatelessMakeHandler::new(Arc::new(
         handlers::DfSessionService::new(),
     )));
-    // We have not implemented extended query in this server, use placeholder instead
-    let placeholder = Arc::new(StatelessMakeHandler::new(Arc::new(
-        PlaceholderExtendedQueryHandler,
-    )));
     let authenticator = Arc::new(StatelessMakeHandler::new(Arc::new(NoopStartupHandler)));
 
     let server_addr = "127.0.0.1:5432";
@@ -28,14 +23,13 @@ async fn main() {
         let incoming_socket = listener.accept().await.unwrap();
         let authenticator_ref = authenticator.make();
         let processor_ref = processor.make();
-        let placeholder_ref = placeholder.make();
         tokio::spawn(async move {
             process_socket(
                 incoming_socket.0,
                 None,
                 authenticator_ref,
+                processor_ref.clone(),
                 processor_ref,
-                placeholder_ref,
             )
             .await
         });
