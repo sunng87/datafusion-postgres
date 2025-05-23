c0cd63df7363abcfe66f6ab1db68998e2700d6ac
Satyam Singh <trueleo@protonmail.com>
2025-05-23 15:44:16 +0530

    Don't recreate parquet file in the test run

--- tests-integration/test.sh
+++ tests-integration/test.sh
@@ -9,7 +9,6 @@ sleep 3
 python tests-integration/test.py
 kill -9 $PID 2>/dev/null
 
-cd tests-integration && python create_arrow_testfile.py && cd .. 
 ./target/debug/datafusion-postgres-cli --parquet all_types:tests-integration/all_types.parquet &
 PID=$!
 sleep 3
